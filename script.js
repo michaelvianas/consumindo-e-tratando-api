4280249f93e75e74ae4bb5de62ae5ed8b0526616
michaelvianas <vmichael116@gmail.com>
2023-04-21 11:25:40 -0300

    Manipulando formulários

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-async function buscarEndereco(cep){
-  try {
-    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    var consultaCEPConvertida = await consultaCEP.json();
-    if(consultaCEPConvertida.erro){
-      throw new Error(`CEP não existente!`);
-    }
+async function buscarEndereco(cep) {
+    let mensagemDeErro = document.getElementById('erro');
+    mensagemDeErro.innerHTML = "";
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        let consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        let cidade = document.getElementById('cidade');
+        let logradouro = document.getElementById('endereco');
+        let estado = document.getElementById('estado');
 
-    return consultaCEPConvertida;
-  } catch(erro) {
-    console.log(erro);
-  }
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemDeErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+        console.log(erro);
+    }
 }
 
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscarEndereco(cep.value));
+/* --- Lidando com várias requisições ao mesmo tempo com Promise.all ---
+
 let ceps = ['01001000', '01001001'];
-let conjuntoCEPS = ceps.map(valores => buscarEndereco(valores));
-//buscarEndereco();
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
 
-Promise.all(conjuntoCEPS).then(respostas => console.log(respostas));
-
+*/
