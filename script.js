3b4fd863feb3174465a079de2c3b4d82c32d5521
michaelvianas <vmichael116@gmail.com>
2023-04-21 09:39:26 -0300

    Criando funções assíncronas

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/65110000/json/');
+async function buscarEndereco(cep){
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if(consultaCEPConvertida.erro){
+      throw new Error(`CEP não existente!`);
+    }
 
-console.log(consultaCEP);
+    return consultaCEPConvertida;
+  } catch(erro) {
+    console.log(erro);
+  }
+}
+
+let ceps = ['01001000', '01001001'];
+let conjuntoCEPS = ceps.map(valores => buscarEndereco(valores));
+//buscarEndereco();
+
+Promise.all(conjuntoCEPS).then(respostas => console.log(respostas));
+
